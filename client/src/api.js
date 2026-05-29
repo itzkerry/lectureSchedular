1a8b6d5892fd70c452463bca1e70042637b78a2d
itzkerry <mauryakrishna098@gmail.com>
2026-05-30 03:38:49 +0530

    backend api link inserted

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://lectureschedular-n3p6.onrender.com/api",
   withCredentials: true,
 
   headers: {
